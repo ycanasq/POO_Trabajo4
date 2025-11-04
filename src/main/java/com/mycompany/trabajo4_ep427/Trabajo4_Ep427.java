39dd3acdf6a226145038e9b96c9b36339b29a798
MacBook <MacBook@MacBook-Pro.local>
2025-11-04 10:48:21 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/trabajo4_ep427/Trabajo4_Ep427.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo4_ep427;
+
+/**
+ *
+ * @author MacBook
+ */
+public class Trabajo4_Ep427 {
+
+    public static void main(String[] args) throws Exception {
+        VentanaPrincipal formulario = new VentanaPrincipal();
+        formulario.setVisible(true);
+        formulario.setResizable(false);
+        
+    }
+}
