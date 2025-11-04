305e16bb273c8bcf5e7ad71ebe6680ac09591e16
ycanasq <71304352+ycanasq@users.noreply.github.com>
2025-11-04 16:06:40 -0500

    Ejercicio400

---
+++ Trabajo4_Ep400.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo4_ep400;
+
+/**
+ *
+ * @author MacBook
+ */
+public class Trabajo4_Ep400 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+    }
+}
