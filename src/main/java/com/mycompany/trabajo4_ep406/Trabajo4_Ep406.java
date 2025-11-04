d428425e63a8b79bee1f7a44ced866e049f918d4
MacBook <MacBook@MacBook-Pro.local>
2025-11-04 11:27:42 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/trabajo4_ep406/Trabajo4_Ep406.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo4_ep406;
+
+/**
+ *
+ * @author MacBook
+ */
+public class Trabajo4_Ep406 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+    }
+}
